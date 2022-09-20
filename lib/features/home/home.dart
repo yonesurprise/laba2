042f083ddfac906c917c2b3804ecfb616fc87d5b
yonesurprise <yonesurprise@gmail.com>
2022-09-20 21:03:13 +0300

    added HomePage

---
+++ lib/features/home/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:laba2/features/task1/task1.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => const Task1(),
+                ),
+              );
+            },
+            child: const Text('Task1'),
+          ),
+        ],
+      ),
+    );
+  }
+}
